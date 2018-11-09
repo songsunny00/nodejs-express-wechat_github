20044cfbde949035bdaeb0c7f4f1bb00cc67a035
songsunny00 <1336733448@qq.com>
2018-11-09 17:56:16 +0800

    privacy

--- app/wechat/config.js
+++ app/wechat/config.js
@@ -1,8 +1,8 @@
 var Configs={
 	index:0,
 	txts:['测试','生产'],
-	ids:['wx2dddd92f0c09ee95','wxa62a8c5db3d686ff'],
-	secrets:['e5b2e2e3145627d8e38f1ac1b19cc0ec','ad54b4d1a82a8247dc3f498048a0c614']
+	ids:['**','**'],
+	secrets:['**','**']
 }
 var Globals={
     APP_ID:Configs.ids[Configs.index],
--- app/work/config.js
+++ app/work/config.js
@@ -2,9 +2,9 @@
 var Configs={
 	index:0,
 	txts:['测试','生产'],
-    appids:['wx08646c0b8ebb3189','wx47bbb0a1044f544b'],
+    appids:['**','**'],
 	agentids:['1000003','1000024'],
-	secrets:['Lj9VTgKUSTtRa3-aDFtFD3M0JqfNsT-9355AtX9xm3o','MZzYBEcT3i6DPzbD5PCMf4W_meQpvNZAnbYs-tXYyPQ']
+	secrets:['**','**']
 }
 var Globals={
     APP_ID:Configs.appids[Configs.index],
