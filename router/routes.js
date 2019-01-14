c16641ceb8d7f7b6b8f848622faaad0889aae2b4
songsunny00 <1336733448@qq.com>
2019-01-14 16:13:59 +0800

    img

--- router/routes.js
+++ router/routes.js
@@ -23,21 +23,6 @@ module.exports = function(app) {
   app.use('/service',Service);
   app.use('/',Wechat);
 
-  //wechat
-  // app.get('/getWechatUserInfo', Wechat.getWechatUser);
-  // app.get('/getWechatAccessToken', Wechat.getWechatAccessToken);
-  // app.get('/getWechatConfig', Wechat.getWechatConfig);
-  // app.post('/getLocationDetail', Wechat.getLocationDetail);
-  // app.post('/getWechatAllUser', Wechat.getWechatAllUser);
-  // app.post('/getContactUser', Wechat.getContactUser);
-  // app.post('/setContactUser', Wechat.setContactUser);
-  // app.post('/sendTemplate', multipartMiddleware, Wechat.sendTemplate);
-
-  //app.post('/login_eaju_app_service/oms/user/Login',multipartMiddleware,Tcp.handleLoginRequest);
-  // app.post('/tcp_node/autoLogin', multipartMiddleware, Tcp.autoLogin);
-  // app.post('/tcp_node/getShareInfo', multipartMiddleware, Tcp.getShareList);
-  // app.post('/tcp_node/updateShareInfo', multipartMiddleware, Tcp.updateShareList);
-  // app.post('/tcp_node/setShareInfo', multipartMiddleware, Tcp.setShareList);
   // 404
   app.get('*', function(req, res){
       res.render('404.html', {
